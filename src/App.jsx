aaf8d27d08bf6fb3026d0f0e76fdaf157140c64d
chushi <tingchushi@hotmail.com>
2022-09-07 01:41:22 +0800

    search bar and show coutry by number

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-// import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import { useState, useEffect } from 'react'
 
+import { useState, useEffect } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ 
 function App() {
-  const [country , setCountry] = useState('')
+  const [country , setCountry] = useState('');
   const [iso , setIso] = useState('')
-  const [detail, setDetail] = useState('')
+  const [date, setDate] = useState('')
+  const [active, setActive] = useState('')
+  const [title, setTitle] = useState(" ");
+  const [fruit, setFruit] = useState();
+
+
 
   useEffect(() => {
     const options = {
@@ -18,49 +26,85 @@ function App() {
     fetch('https://covid-19-statistics.p.rapidapi.com/reports', options)
       .then(response => response.json())
       .then((data) => {
+      
         console.log(data)
-      setCountry(data.data[0].region.name)
-      setIso(data.data[0].region.iso)
-      setDetail({data})
-      },[])
-      // .catch(err => console.error(err));
 
-    // const options = {
-    //   method: 'GET',
-    //   headers: {
-    //     'X-RapidAPI-Key': 'f42d8fa1a7msh98395db0e1a5533p179806jsnc430cce8857b',
-    //     'X-RapidAPI-Host': 'covid-19-statistics.p.rapidapi.com'
-    //   }
-    // };
-    
-    // fetch('https://covid-19-statistics.p.rapidapi.com/reports/total?date=2020-04-07', options)
-    //   .then(response => response.json())
-    //   .then(response => console.log(response))
-    //   .catch(err => console.error(err));
+      setCountry(data.data[fruit].region.name)
+      setIso(data.data[fruit].region.iso)
+      setDate(data.data[title].date)
+      setActive(data.data[title].active)
+
+    },[])
      })
-
+    
+    // console.log(data.data[0].region.name)
+    // setSat(sat.data)
+    
   
-    console.log(country)
-    console.log(detail)
+    const handleSelect=(e)=>{
+    console.log(e);
+    setValue(e)
+  }
+
+    const handleClick = () => {
+      console.log("Click");
+      console.log(title);
+    }
+  
+    const handleChange = (event) => {
+      console.log('change');
+      console.log("event",setTitle(event.target.value));
+    }
+
 
       return (
       <div> 
-        {/* <BrowserRouter >
-        <Routes>
-          <Route /> */}
-       <form>
-       <input placeholder='input'></input>
-       <button>{country}</button>
-       </form>
-       <ol>
-          <li>{country} - {iso}</li>
-          {/* <li>{detail}</li> */}
+        {/* {sat} */}
+        <h1>{title}</h1>
+      <input placeholder="input" onChange={handleChange} />
+           {/* <input
+        placeholder="Method A"
+        onBlur={handleChange}
+        defaultValue={title}
+      /> */}
+      <button onClick={handleClick}>Click</button>
+      <div>
+        {date}: {country} - {iso}
+        </div>
+      <li>Active Case: {active}</li>
 
-       </ol>
-        {/* </Routes>
-        </BrowserRouter> */}
+      {/* <div>
+      <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">0</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+      </div>
+  */}
+    <div>
+      <select id="fruits" value={fruit} 
+              onChange={(e) => setFruit(e.target.value)}>
+        <option value="0">AFG</option>
+        <option value="2">Pear</option>
+        <option value="3">Pineapple</option>
+      </select>
+      <h1>Selected Fruit: {fruit}</h1>
+    </div>
       </div>
   )
 }
 
 export default App
+
+// {
+//   data: {
+//     data [0]: region,
+
+//   }
+// }
